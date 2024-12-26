8f2b785e676c15b896807343e56eab29c08b6586
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-26 14:09:14 +0300

    feat : create function insert for TypeRam

--- src/model/composant/ram/TypeRam.java
+++ src/model/composant/ram/TypeRam.java
@@ -1,9 +1,48 @@
 package model.composant.ram;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.utils.Database;
+
 public class TypeRam {
     
     private int idTypeRam; 
     private String nomTypeRam;
+
+    // CRUD
+    public void insert(Connection c) throws SQLException {
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null;
+        String query = "INSERT INTO type_ram(nom_type_ram) VALUES (?)";
+        
+        try {
+            if(c == null){
+                c = Database.getConnection();
+                isNewConnection = true;
+            }
+            c.setAutoCommit(false);
+            prstm = c.prepareStatement(query);
+            prstm.setString(1, this.getNomTypeRam());
+            
+            prstm.executeUpdate();
+
+            c.commit();
+        } catch (SQLException e) {
+            if (c != null) {
+                c.rollback();
+            }
+            throw e;
+        } finally {
+            if (prstm != null) {
+                prstm.close();
+            }
+            if (c != null && isNewConnection) {
+                c.close();
+            }
+        }
+    }
 
     // GETTERS AND SETTERS
     public int getIdTypeRam() {
