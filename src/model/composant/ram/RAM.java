65b912d7e6dad964ff4899b7fcdb218d2c0793f9
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-26 14:31:06 +0300

    feat : create class RAM

---
+++ src/model/composant/ram/RAM.java
@@ -0,0 +1,22 @@
+package model.composant.ram;
+
+public class RAM {
+    
+    private int idRam; 
+    private boolean portable;
+
+
+    // GETTERS AND SETTERS
+    public int getIdRam() {
+        return idRam;
+    }
+    public boolean isPortable() {
+        return portable;
+    }
+    public void setIdRam(int idRam) {
+        this.idRam = idRam;
+    }
+    public void setPortable(boolean portable) {
+        this.portable = portable;
+    }
+}
