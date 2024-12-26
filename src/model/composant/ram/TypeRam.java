3dc9753c7604b68120a45d9bceccb61254c25272
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-26 14:02:41 +0300

    feat : create class TypeRam

---
+++ src/model/composant/ram/TypeRam.java
@@ -0,0 +1,22 @@
+package model.composant.ram;
+
+public class TypeRam {
+    
+    private int idTypeRam; 
+    private String nomTypeRam;
+
+    // GETTERS AND SETTERS
+    public int getIdTypeRam() {
+        return idTypeRam;
+    }
+    public String getNomTypeRam() {
+        return nomTypeRam;
+    }
+
+    public void setIdTypeRam(int idTypeRam) {
+        this.idTypeRam = idTypeRam;
+    }
+    public void setNomTypeRam(String nomTypeRam) {
+        this.nomTypeRam = nomTypeRam;
+    }
+}
