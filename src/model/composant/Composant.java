9002b8186f40e0bfe819cf12ca5b8e8f43006dbc
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-26 13:49:57 +0300

    feat : create function insertComposant

--- src/model/composant/Composant.java
+++ src/model/composant/Composant.java
@@ -1,10 +1,36 @@
 package model.composant;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Composant {
     private int idComposant;
     private String nomComposant; 
     private double capacite; 
     private double prixUnitaire;
+
+    // CRUD Composant
+    public void insertComposant(Connection connection) throws SQLException {
+        String sql = "INSERT INTO composant (nom_composant, capacite, prix_unitaire) VALUES (?, ?, ?)";
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, this.getNomComposant());
+            pstmt.setDouble(2, this.getCapacite());
+            pstmt.setDouble(3, this.getPrixUnitaire());
+            
+            pstmt.executeUpdate();
+            
+            // Récupération de l'ID généré
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    this.setIdComposant(generatedKeys.getInt(1));
+                }
+            }
+        }
+    }
 
     // GETTERS AND SETTERS
     public int getIdComposant() {
@@ -19,7 +45,7 @@ public class Composant {
     public double getPrixUnitaire() {
         return prixUnitaire;
     }
-    
+
     public void setIdComposant(int idComposant) {
         this.idComposant = idComposant;
     }
