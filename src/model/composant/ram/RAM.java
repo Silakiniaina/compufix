c317e8ba41ebc4e71e6430f482efafc87448b594
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-26 14:32:41 +0300

    feat : add typeRam on RAM

--- src/model/composant/ram/RAM.java
+++ src/model/composant/ram/RAM.java
@@ -1,9 +1,13 @@
 package model.composant.ram;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class RAM {
     
     private int idRam; 
     private boolean portable;
+    private TypeRam typeRam;
 
 
     // GETTERS AND SETTERS
@@ -12,6 +16,13 @@ public class RAM {
     }
     public boolean isPortable() {
         return portable;
+    }
+    public TypeRam getTypeRam(){
+        return this.typeRam;
+    }
+
+    public void setTypeRam(Connection c, int id)throws SQLException{
+        this.typeRam = new TypeRam().getById(c, id);
     }
     public void setIdRam(int idRam) {
         this.idRam = idRam;
