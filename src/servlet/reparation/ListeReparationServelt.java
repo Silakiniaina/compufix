216fca3a7b7c33f7a60271338e0f31145c994d56
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-07 15:33:04 +0300

    feat : create servlet ListReparation Servlet

---
+++ src/servlet/reparation/ListeReparationServelt.java
@@ -0,0 +1,53 @@
+package servlet.reparation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.composant.Composant;
+import model.composant.TypeComposant;
+import model.composant.ram.RAM;
+import model.composant.ram.TypeRam;
+import model.reparation.Reparation;
+
+@WebServlet("/reparations")
+public class ListeReparationServelt extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String typeComposant = (String)req.getAttribute("type");
+        try {
+            Connection c = (Connection)req.getSession().getAttribute("connection");
+
+            List<TypeComposant> types = new TypeComposant().getAll(c);
+            List<Reparation> reparations = new Reparation().getAll(c);
+
+            if(typeComposant != null){
+                int idTypeComposant = Integer.parseInt(typeComposant);
+
+                reparations = new Reparation().getAllByTypeComposant(c, idTypeComposant);
+            }
+
+            req.setAttribute("types", types);
+            req.setAttribute("reparations", reparations);
+            req.setAttribute("pageUrl", "/WEB-INF/views/reparation/liste.jsp");
+            req.getRequestDispatcher("/WEB-INF/views/shared/layout.jsp").forward(req, resp);;
+        } catch (Exception e) {
+            e.printStackTrace(out);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.setAttribute("type", req.getParameter("type"));
+       doGet(req, resp);
+    }
+    
+}
