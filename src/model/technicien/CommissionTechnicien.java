58b529587422c7d710617823a38b29ceb0ff6a89
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-21 15:10:17 +0300

    feat : create function getAllCommissions

--- src/model/technicien/CommissionTechnicien.java
+++ src/model/technicien/CommissionTechnicien.java
@@ -1,7 +1,14 @@
 package model.technicien;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.reparation.Retour;
+import model.utils.Database;
 
 public class CommissionTechnicien {
     
@@ -10,6 +17,50 @@ public class CommissionTechnicien {
     private double totalCommission; 
     private int nombreReparation;
 
+/// ACTION
+    public List<CommissionTechnicien> getAllCommissions(Connection c, CommissionPeriodFilter filter)throws SQLException{
+        List<CommissionTechnicien> results = new ArrayList<>();
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null; 
+        ResultSet rs = null; 
+        String sql = filter != null ? filter.getQuery() : "SELECT id_technicien,SUM(prix) AS total_reparation,SUM(commission_technicien) AS total_commission,COUNT(id_reparation) AS nombre_reparation FROM v_commission_technicien_complet GROUP BY id_technicien ORDER BY total_commission DESC";
+        try {
+            if( c == null){
+                c = Database.getConnection();
+                isNewConnection = true;
+            }
+
+            prstm = c.prepareStatement(sql);
+            int idArg = 1;
+            if(filter != null){
+                if(filter.getDebut() != null){
+                    prstm.setDate( idArg, filter.getDebut());
+                    idArg++;
+                }
+                if(filter.getFin() != null){
+                    prstm.setDate( idArg, filter.getFin());
+                    idArg++;
+                }
+            }
+
+            rs = prstm.executeQuery();
+            while(rs.next()){
+                CommissionTechnicien commission = new CommissionTechnicien();
+                commission.setTechnicien(c, rs.getInt("id_technicien"));
+                commission.setTotalReparation(rs.getDouble("total_reparation"));
+                commission.setTotalCommission(rs.getDouble("total_commission"));
+                commission.setNombreReparation(rs.getInt("nombre_reparation"));
+
+                results.add(commission);
+            }
+
+            return results;
+        } catch (SQLException e) {
+            throw e;
+        } finally{
+            Database.closeRessources(rs, prstm, c, Boolean.valueOf(isNewConnection));
+        }
+    }
 
 
 /// GETTERS AND SETTERS
