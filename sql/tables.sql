c9e009d210f245232e2f3b8bc504ef2273cddcec
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-01-30 13:24:40 +0300

    fix : fix table

--- sql/tables.sql
+++ sql/tables.sql
@@ -16,81 +16,6 @@ CREATE TABLE composant(
    FOREIGN KEY(id_type_composant) REFERENCES type_composant(id_type_composant)
 );
 
--- CREATE TABLE type_ram(
---    id_type_ram SERIAL,
---    nom_type_ram VARCHAR(20)  NOT NULL,
---    PRIMARY KEY(id_type_ram),
---    UNIQUE(nom_type_ram)
--- );
-
--- CREATE TABLE RAM(
---    id_ram SERIAL,
---    est_portable BOOLEAN DEFAULT true,
---    id_type_ram INTEGER NOT NULL,
---    id_composant INTEGER NOT NULL,
---    PRIMARY KEY(id_ram),
---    FOREIGN KEY(id_type_ram) REFERENCES type_ram(id_type_ram),
---    FOREIGN KEY(id_composant) REFERENCES composant(id_composant)
--- );
-
--- CREATE TABLE type_processeur(
---    id_type_processeur SERIAL,
---    nom_type_processeur VARCHAR(10)  NOT NULL,
---    PRIMARY KEY(id_type_processeur)
--- );
-
--- CREATE TABLE processeur(
---    id_processeur SERIAL,
---    generation SMALLINT NOT NULL,
---    nombre_coeur SMALLINT NOT NULL,
---    id_type_processeur INTEGER,
---    id_composant INTEGER NOT NULL,
---    PRIMARY KEY(id_processeur),
---    FOREIGN KEY(id_type_processeur) REFERENCES type_processeur(id_type_processeur),
---    FOREIGN KEY(id_composant) REFERENCES composant(id_composant)
--- );
-
--- CREATE TABLE type_disque(
---    id_type_disque SERIAL,
---    nom_type_disque VARCHAR(10)  NOT NULL,
---    PRIMARY KEY(id_type_disque)
--- );
-
--- CREATE TABLE disque_dur(
---    id_disque_dur SERIAL,
---    est_portable BOOLEAN DEFAULT false,
---    id_type_disque INTEGER NOT NULL,
---    id_composant INTEGER NOT NULL,
---    PRIMARY KEY(id_disque_dur),
---    FOREIGN KEY(id_type_disque) REFERENCES type_disque(id_type_disque),
---    FOREIGN KEY(id_composant) REFERENCES composant(id_composant)
--- );
-
--- CREATE TABLE carte_mere(
---    id_carte_mere SERIAL,
---    nombre_slot_ram SMALLINT NOT NULL,
---    nombre_slot_disque SMALLINT NOT NULL,
---    id_type_disque INTEGER NOT NULL,
---    id_type_processeur INTEGER NOT NULL,
---    id_type_ram INTEGER NOT NULL,
---    id_composant INTEGER NOT NULL,
---    PRIMARY KEY(id_carte_mere),
---    FOREIGN KEY(id_type_disque) REFERENCES type_disque(id_type_disque),
---    FOREIGN KEY(id_type_processeur) REFERENCES type_processeur(id_type_processeur),
---    FOREIGN KEY(id_type_ram) REFERENCES type_ram(id_type_ram),
---    FOREIGN KEY(id_composant) REFERENCES composant(id_composant)
--- );
-
--- CREATE TABLE carte_mere_utilisation(
---    id_composant INTEGER,
---    id_carte_mere INTEGER,
---    type_slot VARCHAR(20)  NOT NULL,
---    date_installation VARCHAR(50)  DEFAULT NOW( ) ,
---    PRIMARY KEY(id_composant, id_carte_mere),
---    FOREIGN KEY(id_composant) REFERENCES composant(id_composant),
---    FOREIGN KEY(id_carte_mere) REFERENCES carte_mere(id_carte_mere)
--- );
-
 -- stock --
 CREATE TABLE mouvement_stock(
    id_mouvement_stock SERIAL,
@@ -152,7 +77,6 @@ CREATE TABLE reparation(
    FOREIGN KEY(id_ordinateur) REFERENCES ordinateur(id_ordinateur)
 );
 
-
 CREATE TABLE composant_reparation(
    id_reparation INTEGER,
    id_composant_ordinateur INTEGER,
@@ -166,7 +90,6 @@ CREATE TABLE composant_reparation(
 
 CREATE TABLE retour(
    id_retour SERIAL,
-   prix NUMERIC(18,2)   NOT NULL CHECK(prix >= 0),
    date_retour DATE DEFAULT NOW( ),
    id_reparation INTEGER NOT NULL,
    PRIMARY KEY(id_retour),
